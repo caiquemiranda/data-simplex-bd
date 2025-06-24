369d5681a1e76a14fd82c4f55c1b10d03390acaf
Caique Miranda <96856440+caiquemiranda@users.noreply.github.com>
2025-06-24 00:50:29 +0000

    start code menu

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 > esse projeto é um workflow de vizualização dos dados de paineis simplex johnsons control (4010, 4100, 4007)
 
 > a ideia é exportar os dados do ESprogrammer e gerar as visualizações. A baixo colocarei uma prévia da viz.
+
+> run: ```python -m streamlit run src/app.py```
--- src/app.py
+++ src/app.py
@@ -1,3 +1,20 @@
 import streamlit as st
+import time
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+import snowflake.connector
+import streamlit_option_menu
+from streamlit_option_menu import option_menu
 
 
+with st.sidebar:
+    selected = option_menu(
+    menu_title = "Main Menu",
+    options = ["Home","Disponibilidade","Status"],
+    icons = ["house","gear","activity"],
+    menu_icon = "cast",
+    default_index = 0,
+    #orientation = "horizontal",
+)
