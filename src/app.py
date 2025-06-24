6b8761da0635a9b6e4e3e607e4f4c92247798c13
Caique Miranda <96856440+caiquemiranda@users.noreply.github.com>
2025-06-24 01:02:51 +0000

    update app.py wt select

--- src/app.py
+++ src/app.py
@@ -20,29 +20,10 @@ with st.sidebar:
 )
 
 if selected == "Upload dados":
-    c1, c2= st.columns(2)
-    c3, c4= st.columns(2)
+    st.header('Faça o Upload dos dados aqui:')
 
-    with st.container():
-        c1.write("c1")
-        c2.write("c2")
+if selected == "Viz BD":
+    st.header('Aqui voce encontra os Bancos')
 
-    with st.container():
-        c3.write("c3")
-        c4.write("c4")
-
-    with c1:
-        chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
-        st.area_chart(chart_data)
-           
-    with c2:
-        chart_data = pd.DataFrame(np.random.randn(20, 3),columns=["a", "b", "c"])
-        st.bar_chart(chart_data)
-
-    with c3:
-        chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
-        st.line_chart(chart_data)
-
-    with c4:
-        chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
-        st.line_chart(chart_data)
+if selected == "Historico":
+    st.header('Aqui voce encontra os cards')
