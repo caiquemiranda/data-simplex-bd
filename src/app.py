9b690613eff30981b72e276dfdfb735bf289b19a
Caique Miranda <96856440+caiquemiranda@users.noreply.github.com>
2025-06-24 00:56:10 +0000

    update app.py

--- src/app.py
+++ src/app.py
@@ -1,20 +1,48 @@
 import streamlit as st
-import time
+#import time
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
-import snowflake.connector
-import streamlit_option_menu
+#import snowflake.connector
+#import streamlit_option_menu
 from streamlit_option_menu import option_menu
 
 
 with st.sidebar:
     selected = option_menu(
     menu_title = "Main Menu",
-    options = ["Home","Disponibilidade","Status"],
+    options = ["Upload dados","Viz BD","Historico"],
     icons = ["house","gear","activity"],
     menu_icon = "cast",
     default_index = 0,
     #orientation = "horizontal",
 )
+
+if selected == "Upload dados":
+    c1, c2= st.columns(2)
+    c3, c4= st.columns(2)
+
+    with st.container():
+        c1.write("c1")
+        c2.write("c2")
+
+    with st.container():
+        c3.write("c3")
+        c4.write("c4")
+
+    with c1:
+        chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
+        st.area_chart(chart_data)
+           
+    with c2:
+        chart_data = pd.DataFrame(np.random.randn(20, 3),columns=["a", "b", "c"])
+        st.bar_chart(chart_data)
+
+    with c3:
+        chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
+        st.line_chart(chart_data)
+
+    with c4:
+        chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
+        st.line_chart(chart_data)
