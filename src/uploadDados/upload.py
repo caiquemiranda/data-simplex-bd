073febfa78a042bf1136e8a82764cdbfb672927b
Caique Miranda <96856440+caiquemiranda@users.noreply.github.com>
2025-06-27 01:18:47 +0000

    corrige a ordem das funções e ajusta o tratamento de upload para exibir o arquivo CSV selecionado

--- src/uploadDados/upload.py
+++ src/uploadDados/upload.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
 
-def tratamentoUpload():
-    file = st.file_uploader("Escolha um arquivo CSV", type=["csv"])
-    df = pd.read_csv(file)
-    return st.dataframe(df)
-
 def uploadDados():
     return st.header('Faça o Upload dos dados aqui:')
+
+def tratamentoUpload():
+    file = st.file_uploader("Escolha um arquivo CSV", type=["csv"])
+    #df = pd.read_csv(file)
+    return st.dataframe(file)
